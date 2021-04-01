6f10fea3b62718ad3c500dcbcd8f5254894376c0
Hugo Sales <hugo@hsal.es>
2021-04-01 19:08:43 +0000

    Throw RedisException instead of the general Exception

--- src/Redis.php
+++ src/Redis.php
@@ -5,7 +5,7 @@ if (!class_exists('Redis')) {
     {
         public function __construct()
         {
-            throw new Exception("This polyfill doesn't actually implement anything, it's purpose is to allow for optional use in the symfony framework");
+            throw new RedisException("This polyfill doesn't actually implement anything, it's purpose is to allow for optional use in the symfony framework");
         }
     }
 }
--- src/RedisException.php
+++ src/RedisException.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!class_exists('RedisException')) {
-    class RedisException extends Redis
+    class RedisException extends Exception
     {
     }
 }
